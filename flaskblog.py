bc3fe06c03c3c525ac7f738c805614be24c330dd
Junsoo-cloud <kfxsmt1@naver.com>
2023-01-26 14:48:22 +0000

    flaskblog

--- flaskblog.py
+++ flaskblog.py
@@ -1,58 +1,82 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-import sys 
-app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'd6e2fcf20403bfea892f8ad76d455690'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 
 posts = [
-  {
-    'author': 'Junsoo',
-    'title': 'Blog Post1',
-    'content': 'First post content',
-    'date_posted': 'April 20, 2020'
-  },
-  
-  { 
-    
-    'autor': 'seung wu',
-    'title': 'Blog Post2',
-    'content': 'Second post content',
-    'data_posted': 'April 22, 2020' 
-  } 
-  
-  
-    
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
 ]
 
-@app.route('/')
 
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
-    return render_template('home.html',posts = posts)
-  
-@app.route('/about')
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm() # Create Instance
+    form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success') 
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = LoginForm() # Create Instance
+    form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in !', 'success')
+            flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger') # Bootstrap -> danger : Red Alert
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
@@ -62,3 +86,5 @@ if __name__ == "__main__":
 
 
 # Part 3 - Forms and User Input : WT forms
+
+    # backref -> author만으로 user식별 , lazy = SQLAlchemy에서 in one에 가져올 수 있다
