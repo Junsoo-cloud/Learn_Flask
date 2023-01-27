5cfbc64b6594db035c0d5090fc3ae23dde30852f
Junsoo-cloud <kfxsmt1@naver.com>
2023-01-27 14:26:37 +0000

    flask_tutorial

--- flaskblog.py
+++ flaskblog.py
@@ -2,11 +2,15 @@ from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-
+# 6번 23분까지 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
 
 
 class User(db.Model):
@@ -47,6 +51,9 @@ posts = [
     }
 ]
 
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 @app.route("/home")
